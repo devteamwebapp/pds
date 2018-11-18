ac63db53114bf4c801408cf3d9503e59cb5669af
devteamwebapp <devteamwebapp@gmail.com>
2018-11-18 19:44:01 +0100

    Initial commit

---
+++ src/main/java/com/devteam/pds/PdsApplication.java
@@ -0,0 +1,12 @@
+package com.devteam.pds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdsApplication.class, args);
+    }
+}
